7a24973bb29cbe7e138bf01045f0348babdb08c5
SF-218 <sahlatulfarihah@gmail.com>
2022-04-20 00:08:41 +0800

    changes in key

--- 18059938/script_dir/18059938_script.sh
+++ 18059938/script_dir/18059938_script.sh
@@ -7,5 +7,5 @@ puppet resource file /18059938/clone ensure=absent force=true
 puppet resource file /18059938 ensure=directory
 puppet resource file /18059938/clone ensure=directory
 cd /18059938/clone
-git clone https://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@github.com/SF-218/operate_repo.git
+git clone https://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@github.com/SF-218/operate_repo.git
 cp -p /18059938/operate_repo/index.html /var/www/html
